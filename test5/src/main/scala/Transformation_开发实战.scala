3e71b2b606d09102de255b963dc7e35978c10119
hongyunshui <jianhong513@163.com>
2018-11-14 11:52:24 +0800

    groupByKey 操作

--- test5/src/main/scala/Transformation_开发实战.scala
+++ test5/src/main/scala/Transformation_开发实战.scala
@@ -21,12 +21,14 @@ object Transformation_开发实战{
     tr.filter_test()
     println("***********flatMap test**********")
     tr.flatMap_test()
+    println("******groupByKey操作*******")
+    tr.groupByKey_test()
   }
 }
 class Transformation_开发实战(){
   /**
     * map算子案例：将集合中每一个元素都乘以2
-    * map算子是对任何类型的RDD，都可以调用
+    * map算子： 是对任何类型的RDD，都可以调用
     */
   def map_test(): Unit ={
     // 构造集合
@@ -41,7 +43,7 @@ class Transformation_开发实战(){
 
   /**
     * filter 过滤到集合中的偶数
-    * filter 算子传入的是一个function，如果function返回的是true则保留当前元素，否则就不保留。
+    * filter算子： 算子传入的是一个function，如果function返回的是true则保留当前元素，否则就不保留。
     */
   def filter_test(): Unit ={
     // 构造集合
@@ -55,7 +57,7 @@ class Transformation_开发实战(){
   }
 
   /**
-    *
+    *flatMap算子： 即将接收的原始RDD中的每个元素进行逻辑的计算和处理，返回多个元素，新的RDD中封装了所有的新元素
     */
   def flatMap_test(): Unit ={
     // 构造集合
@@ -66,5 +68,21 @@ class Transformation_开发实战(){
     val words = linesRDD.flatMap(lines => lines.split(" "))
     // 打印结果
     words.foreach( word => println(word))
+  }
+
+  /**
+    * groupByKey算子:
+    * 案例：按照班级对乘积分组
+    */
+  def groupByKey_test(): Unit ={
+    // 模拟集合
+    val scores = Array(("class1", 80),("class2", 90),("class1", 75),("class3", 95), ("class6", 70))
+    // 并行化集合，创建初始化RDD
+    val scoresRDD = Transformation_开发实战.sc.parallelize(scores)
+    // 对scoresRDD 进行groupByKey操作
+    val groupScores = scoresRDD.groupByKey()
+    // 打印结果
+    groupScores.foreach(classScores => println(classScores))
+
   }
 }
